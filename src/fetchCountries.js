289450ac2c385a05b0f5ee9b22e204e75faaf8cb
Aelita Deinychenko <amm-aelita@ukr.net>
2023-02-15 08:50:25 +0200

    Create fetchCountries.js

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+export { fetchCountries };
+
+const URL = `https://restcountries.com/v3.1/name/';
+
+const fetchCountries = name => {
+    return fetch(`${ URL }/${name}?fields = name,capital,population,flags,languages`)
+    .then(response => {
+        if (response.status === 404) {
+            return Promise.reject(new Error());
+        } return response.json();
+    },
+    );
+}
