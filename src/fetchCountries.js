3aef3ff5d8007cd2889cc1139c92e016ccfea891
Aelita Deinychenko <amm-aelita@ukr.net>
2023-02-15 09:14:09 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,9 @@
 export { fetchCountries };
 
-const BASE_URL = 'https://restcountries.com/v3.1/name'
+const URL = 'https://restcountries.com/v3.1/name'
 
 const fetchCountries = name => {
-    return fetch(`${BASE_URL}/${name}?fields = name,capital,population,flags,languages`).then(response => {
+    return fetch(`${URL}/${name}?fields = name,capital,population,flags,languages`).then(response => {
         if (response.status === 404) {
             return Promise.reject(new Error());
         } return response.json();
