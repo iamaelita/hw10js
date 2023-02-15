0891377edf33473e4c7956ef4c6c308a46f7efe7
Aelita Deinychenko <amm-aelita@ukr.net>
2023-02-15 09:25:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,70 @@ import { Notify } from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 
 
+const refs = {
+      input: document.getElementById('search-box'),
+      list: document.querySelector('.country-list'),
+      info: document.querySelector('.country-info'),
+    };
+
+const cleanMarkup = ref => (ref.innerHTML = '');
+
+const inputHandler = e => {
+  const textInput = e.target.value.trim();
+
+  if (!textInput) {
+    cleanMarkup(list;
+    cleanMarkup(info);
+    return;
+  }
+
+  fetchCountries(textInput)
+    .then(data => {
+      console.log(data);
+      if (data.length > 10) {
+        Notify.info('Занадто багато співпадінь. Введіть більше інформації!');
+        return;
+      }
+      renderMarkup(data);
+    })
+    .catch(err => {
+      cleanMarkup(list);
+      cleanMarkup(info);
+      Notify.failure('Така країна відсутня!');
+    });
+};
+
+const renderMarkup = data => {
+  if (data.length === 1) {
+    cleanMarkup(list);
+    const markupInfo = createInfoMarkup(data);
+    info.innerHTML = markupInfo;
+  } else {
+    cleanMarkup(info);
+    const markupList = createListMarkup(data);
+    list.innerHTML = markupList;
+  }
+};
+
+const createListMarkup = data => {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>`,
+    )
+    .join('');
+};
+
+const createInfoMarkup = data => {
+  return data.map(
+    ({ name, capital, population, flags, languages }) =>
+      `<h1><img src="${flags.png}" alt="${name.official}" width="40" height="40">${
+        name.official
+      }</h1>
+      <p>Capital: ${capital}</p>
+      <p>Population: ${population}</p>
+      <p>Languages: ${Object.values(languages)}</p>`,
+  );
+};
+
+input.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
