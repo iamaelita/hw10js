5d321b2e8d11b60fc34d8ae7bb01e5a7f63480fc
Aelita Deinychenko <amm-aelita@ukr.net>
2023-02-15 09:00:03 +0200

    import libs into js-file

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -11,3 +11,4 @@ const fetchCountries = name => {
     },
     );
 }
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
-
+import { fetchCountries } from './fetchCountries.js';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+    
 const DEBOUNCE_DELAY = 300;
+
+
